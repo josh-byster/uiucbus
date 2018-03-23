8105c221cafaa9470c9d7cfd3ec2b8e6fc00b3e0
Josh Byster <joshb1050@gmail.com>
2018-03-23 03:25:50 -0500

    WebStorm reformat

--- gtag.js
+++ gtag.js
@@ -1,21 +1,21 @@
-
 window.dataLayer = window.dataLayer || [];
 
 function gtag() {
-  dataLayer.push(arguments);
+    dataLayer.push(arguments);
 }
+
 gtag('js', new Date());
 
 gtag('config', 'UA-109186351-1');
 
 
 window._urq = window._urq || [];
-    _urq.push(['initSite', '96cc2c62-7395-4573-8001-5b852e8d380b']);
-    (function() {
-      var ur = document.createElement('script');
-      ur.type = 'text/javascript';
-      ur.async = true;
-      ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(ur, s);
-    })();
+_urq.push(['initSite', '96cc2c62-7395-4573-8001-5b852e8d380b']);
+(function () {
+    var ur = document.createElement('script');
+    ur.type = 'text/javascript';
+    ur.async = true;
+    ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ur, s);
+})();
--- js/index.js
+++ js/index.js
@@ -1,86 +1,85 @@
+Raven.config('https://a3cd776e555b4f62b9215dee5e11a886@sentry.io/306037').install();
 
-  Raven.config('https://a3cd776e555b4f62b9215dee5e11a886@sentry.io/306037').install();
+var cumtd_api = my_api_keys.CUMTD_API_KEY;
+var storedAry = [];
 
-  var cumtd_api = my_api_keys.CUMTD_API_KEY;
-  var storedAry = [];
-
-  function navbarFunc(index) {
-    Raven.context(function() {
-      var arr = [
-        ["PLAZA", "Transit Plaza"],
-        ["IU", "Illini Union"],
-        ["PAR", "Pennsylvania Ave. Residence Hall"],
-        ["KRANNERT", "Krannert Center"],
-        ["1STSTDM", "First and Stadium"],
-        ['GWNNV', 'Goodwin and Nevada'],
-        ['ARC', 'Activities and Recreation']
-      ];
-      storedAry.push(arr[index]);
-      Cookies.set('storedAry', JSON.stringify(storedAry), {
-        expires: 10
-      });
-      window.location = `BusTracking.html?id=${arr[index][0]}&name=${encodeURIComponent(arr[index][1])}&refreshcount=0`
+function navbarFunc(index) {
+    Raven.context(function () {
+        var arr = [
+            ["PLAZA", "Transit Plaza"],
+            ["IU", "Illini Union"],
+            ["PAR", "Pennsylvania Ave. Residence Hall"],
+            ["KRANNERT", "Krannert Center"],
+            ["1STSTDM", "First and Stadium"],
+            ['GWNNV', 'Goodwin and Nevada'],
+            ['ARC', 'Activities and Recreation']
+        ];
+        storedAry.push(arr[index]);
+        Cookies.set('storedAry', JSON.stringify(storedAry), {
+            expires: 10
+        });
+        window.location = `BusTracking.html?id=${arr[index][0]}&name=${encodeURIComponent(arr[index][1])}&refreshcount=0`
     });
-  }
+}
 
 
-  function setUpStoredAry() {
+function setUpStoredAry() {
     var storedAry = Cookies.get("storedAry");
     if (storedAry !== undefined && storedAry.length > 2) {
-      storedAry = JSON.parse(storedAry);
-      document.getElementById('recents').innerHTML = "<h3>Recently Viewed:</h3>"
-      for (i = storedAry.length - 1; i >= Math.max(storedAry.length, 3) - 3; i--) {
-        var currentElementName = storedAry[i][1];
-        var currentElementName_encoded = encodeURIComponent(currentElementName);
-        var currentElementID = storedAry[i][0];
-        var window_location = `BusTracking.html?id=${currentElementID}&name=${currentElementName_encoded}`;
-        document.getElementById('recents').innerHTML += `<button type="button" class="btn btn-success" style='margin-bottom:20px;' onclick="window.location = '${window_location}';">${currentElementName}</button><br/>`;
+        storedAry = JSON.parse(storedAry);
+        document.getElementById('recents').innerHTML = "<h3>Recently Viewed:</h3>"
+        for (i = storedAry.length - 1; i >= Math.max(storedAry.length, 3) - 3; i--) {
+            var currentElementName = storedAry[i][1];
+            var currentElementName_encoded = encodeURIComponent(currentElementName);
+            var currentElementID = storedAry[i][0];
+            var window_location = `BusTracking.html?id=${currentElementID}&name=${currentElementName_encoded}`;
+            document.getElementById('recents').innerHTML += `<button type="button" class="btn btn-success" style='margin-bottom:20px;' onclick="window.location = '${window_location}';">${currentElementName}</button><br/>`;
 
-      }
-      document.getElementById('recents').innerHTML +=
-        `<button type="button" class="btn btn-primary" style='margin-bottom:20px;' onclick="storedAry=[];Cookies.set('storedAry',[],{expires:10});document.getElementById('recents').innerHTML='';">Clear</button><br/>`;
+        }
+        document.getElementById('recents').innerHTML +=
+            `<button type="button" class="btn btn-primary" style='margin-bottom:20px;' onclick="storedAry=[];Cookies.set('storedAry',[],{expires:10});document.getElementById('recents').innerHTML='';">Clear</button><br/>`;
     }
-  }
+}
 
-  Raven.context(function() {
+Raven.context(function () {
     setUpStoredAry();
-  });
+});
 
-  var options = {
-    url: function(phrase) {
-      return "https://www.cumtd.com/autocomplete/stops/v1.0/json/search?query=" + phrase + "&format=json";
+var options = {
+    url: function (phrase) {
+        return "https://www.cumtd.com/autocomplete/stops/v1.0/json/search?query=" + phrase + "&format=json";
     },
 
     getValue: "n",
 
     list: {
 
-      onChooseEvent: function() {
-        Raven.context(function() {
-          var id = $("#provider-remote").getSelectedItemData().i;
-          var name = $("#provider-remote").getSelectedItemData().n;
-          try {
-            storedAry.push([id, name]);
-            Cookies.set('storedAry', JSON.stringify(storedAry), {
-              expires: 10
+        onChooseEvent: function () {
+            Raven.context(function () {
+                var id = $("#provider-remote").getSelectedItemData().i;
+                var name = $("#provider-remote").getSelectedItemData().n;
+                try {
+                    storedAry.push([id, name]);
+                    Cookies.set('storedAry', JSON.stringify(storedAry), {
+                        expires: 10
+                    });
+                } catch (e) {
+                    Raven.captureException(new Error("Browser cannot to store cookies. Not necessarily error."));
+                    console.log("Hello");
+                }
+
+                window.location = `BusTracking.html?id=${id}&name=${name}`
             });
-          } catch (e) {
-            Raven.captureException(new Error("Browser cannot to store cookies. Not necessarily error."));
-            console.log("Hello");
-          }
-
-          window.location = `BusTracking.html?id=${id}&name=${name}`
-        });
-      }
+        }
     }
-  };
+};
 
-  $("#provider-remote").easyAutocomplete(options);
+$("#provider-remote").easyAutocomplete(options);
 
-  function showModal() {
+function showModal() {
     if (!document.getElementById("location")) {
-      document.body.innerHTML +=
-        `<div class="modal fade" id="location">
+        document.body.innerHTML +=
+            `<div class="modal fade" id="location">
 <div class="modal-dialog" role="document">
   <div class="modal-content">
     <div class="modal-header">
@@ -102,35 +101,35 @@
 </div>`;
     }
     $("#location").modal("show");
-    $('#location').on('hidden.bs.modal', function() {
-      $("#provider-remote").easyAutocomplete(options);
+    $('#location').on('hidden.bs.modal', function () {
+        $("#provider-remote").easyAutocomplete(options);
     })
-  }
+}
 
-  function showModalBefore() {
+function showModalBefore() {
     $('#myModal').modal('show');
     document.getElementById("modalBody").innerHTML = "Loading stops...";
-  }
+}
 
-  function getLocation() {
+function getLocation() {
     if (navigator.geolocation) {
-      showModalBefore();
-      navigator.geolocation.getCurrentPosition(showPosition);
+        showModalBefore();
+        navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-      window.alert("We could not get your location.");
+        window.alert("We could not get your location.");
     }
-  }
+}
 
-  function showPosition(position) {
-    $.getJSON(`https://developer.cumtd.com/api/v2.2/json/getstopsbylatlon?key=${cumtd_api}&pt=60&lat=${position.coords.latitude}&lon=${position.coords.longitude}`, function(data) {
-      console.log(data);
-      modalBody = document.getElementById("modalBody");
-      modalBody.innerHTML = "";
-      for (i = 0; i < 3; i++) {
-        redirectURL = `BusTracking.html?id=${data.stops[i].stop_id}&name=${encodeURIComponent(data.stops[i].stop_name)}`;
-        modalBody.innerHTML += `<a href=${redirectURL}>${data.stops[i].stop_name} (${(data.stops[i].distance*0.00018939).toFixed(2)} mi.)</a></br>`
-        modalBody.innerHTML += ``;
-        console.log(data.stops[i]);
-      }
+function showPosition(position) {
+    $.getJSON(`https://developer.cumtd.com/api/v2.2/json/getstopsbylatlon?key=${cumtd_api}&pt=60&lat=${position.coords.latitude}&lon=${position.coords.longitude}`, function (data) {
+        console.log(data);
+        modalBody = document.getElementById("modalBody");
+        modalBody.innerHTML = "";
+        for (i = 0; i < 3; i++) {
+            redirectURL = `BusTracking.html?id=${data.stops[i].stop_id}&name=${encodeURIComponent(data.stops[i].stop_name)}`;
+            modalBody.innerHTML += `<a href=${redirectURL}>${data.stops[i].stop_name} (${(data.stops[i].distance * 0.00018939).toFixed(2)} mi.)</a></br>`
+            modalBody.innerHTML += ``;
+            console.log(data.stops[i]);
+        }
     });
-  }
+}
