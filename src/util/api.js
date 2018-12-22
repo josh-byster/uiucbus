2cd95cdf4463188793bcef9eadab423d82589720
Josh Byster <joshb1050@gmail.com>
2018-12-22 14:29:27 -0600

    Migrate API to HTTPS

--- src/util/api.js
+++ src/util/api.js
@@ -1,4 +1,4 @@
-var API_URL = "http://developer.cumtd.com/api/v2.2/json/";
+var API_URL = "https://developer.cumtd.com/api/v2.2/json/";
 var MAPBOX_API_KEY =
   "pk.eyJ1Ijoiam9zaC1ieXN0ZXIiLCJhIjoiY2psN2xyZGFoMDY5ZjNxbWtpeDE0dDlwNSJ9.AAJipEPA6e-kLi1Jv3Wpyg";
 var CUMTD_API_KEY = "fd4fb84bbbb34acfae890f17144ee131";
