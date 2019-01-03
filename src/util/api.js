3f49f2409cf4211547b0df1d7c3e58fbfc377667
Josh Byster <joshb1050@gmail.com>
2019-01-03 16:53:54 -0600

    Changed fetch API to axios for Cypress compatibility

--- src/util/api.js
+++ src/util/api.js
@@ -1,39 +1,36 @@
+import axios from "axios";
+
 var API_URL = "https://developer.cumtd.com/api/v2.2/json/";
 var MAPBOX_API_KEY =
   "pk.eyJ1Ijoiam9zaC1ieXN0ZXIiLCJhIjoiY2psN2xyZGFoMDY5ZjNxbWtpeDE0dDlwNSJ9.AAJipEPA6e-kLi1Jv3Wpyg";
 var CUMTD_API_KEY = "fd4fb84bbbb34acfae890f17144ee131";
 
 function getBuses(stop_id) {
-  return fetch(
-    `${API_URL}/getdeparturesbystop?key=${CUMTD_API_KEY}&stop_id=${stop_id}`,
-    {
-      method: "GET"
-    }
-  ).then(res => res.json());
+  return axios
+    .get(
+      `${API_URL}/getdeparturesbystop?key=${CUMTD_API_KEY}&stop_id=${stop_id}`
+    )
+    .then(res => res.data);
 }
 
 // Get the stop name and additional info
 function getStop(stop_id) {
-  return fetch(`${API_URL}/getstop?key=${CUMTD_API_KEY}&stop_id=${stop_id}`, {
-    method: "GET"
-  }).then(res => res.json());
+  return axios
+    .get(`${API_URL}/getstop?key=${CUMTD_API_KEY}&stop_id=${stop_id}`)
+    .then(res => res.data);
 }
 
 function getVehicleInfo(vehicle_id) {
-  return fetch(
-    `${API_URL}/getvehicle?key=${CUMTD_API_KEY}&vehicle_id=${vehicle_id}`,
-    {
-      method: "GET"
-    }
-  ).then(res => res.json());
+  return axios
+    .get(`${API_URL}/getvehicle?key=${CUMTD_API_KEY}&vehicle_id=${vehicle_id}`)
+    .then(res => res.data);
 }
 
 function getNearestStops(latitude, longitude) {
-  return fetch(
-    `${API_URL}/getstopsbylatlon?key=${CUMTD_API_KEY}&lat=${latitude}&lon=${longitude}`,
-    {
-      method: "GET"
-    }
-  ).then(res => res.json());
+  return axios
+    .get(
+      `${API_URL}/getstopsbylatlon?key=${CUMTD_API_KEY}&lat=${latitude}&lon=${longitude}`
+    )
+    .then(res => res.data);
 }
 export { getBuses, getStop, MAPBOX_API_KEY, getVehicleInfo, getNearestStops };
