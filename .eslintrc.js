bef4868bf3fe03b3c55141321a20eb95dec08adf
Josh Byster <joshb1050@gmail.com>
2019-03-23 02:57:11 -0500

    Adding eslint runner

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  rules: {
+    'comma-dangle': 0,
+    'react/forbid-prop-types': 0
+  },
+  parser: 'babel-eslint'
+};
--- src/components/BusInfoModal.jsx
+++ src/components/BusInfoModal.jsx
@@ -1,50 +1,55 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Modal, ModalHeader, ModalBody, Button, ModalFooter } from "reactstrap";
-import { MAPBOX_API_KEY, getVehicleInfo, getStop } from "../util/api.js";
-import "../styles/InfoModal.scss";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Modal, ModalHeader, ModalBody, Button, ModalFooter } from 'reactstrap';
+import { MAPBOX_API_KEY, getVehicleInfo, getStop } from '../util/api';
+import '../styles/InfoModal.scss';
+
 class BusInfoModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mapURL: "",
-      nextStop: "",
-      previousStop: ""
+      mapURL: '',
+      nextStop: '',
+      previousStop: ''
     };
   }
-  componentDidUpdate(prevProps) {
-    if (prevProps.busInfo !== this.props.busInfo) {
-      var mapURL = this.getMapURL();
 
-      this.setState({ mapURL: mapURL, imgLoaded: false });
+  componentDidUpdate(prevProps) {
+    const { busInfo } = this.props;
+    if (prevProps.busInfo !== busInfo) {
+      const mapURL = this.getMapURL();
+
+      // eslint-disable-next-line react/no-did-update-set-state
+      this.setState({ mapURL, imgLoaded: false });
       this.getNextPrevStops();
     }
   }
 
   getNextPrevStops = async () => {
-    const { status, vehicles } = await getVehicleInfo(
-      this.props.busInfo.vehicle_id
-    );
+    const { busInfo } = this.props;
+    const { status, vehicles } = await getVehicleInfo(busInfo.vehicle_id);
+    let nextStopName;
+    let prevStopName;
     if (status.code === 200) {
-      var nextStopID = vehicles[0].next_stop_id;
-      var prevStopID = vehicles[0].previous_stop_id;
+      const nextStopID = vehicles[0].next_stop_id;
+      const prevStopID = vehicles[0].previous_stop_id;
 
       if (nextStopID !== null) {
-        var nextStop = await getStop(nextStopID);
-        var nextStopName = nextStop.stops[0].stop_points.filter(obj => {
-          return obj.stop_id === nextStopID;
-        })[0].stop_name;
+        const nextStop = await getStop(nextStopID);
+        nextStopName = nextStop.stops[0].stop_points.filter(
+          obj => obj.stop_id === nextStopID
+        )[0].stop_name;
       } else {
-        nextStopName = "Unknown";
+        nextStopName = 'Unknown';
       }
 
       if (prevStopID !== null) {
-        var prevStop = await getStop(prevStopID);
-        var prevStopName = prevStop.stops[0].stop_points.filter(obj => {
-          return obj.stop_id === prevStopID;
-        })[0].stop_name;
+        const prevStop = await getStop(prevStopID);
+        prevStopName = prevStop.stops[0].stop_points.filter(
+          obj => obj.stop_id === prevStopID
+        )[0].stop_name;
       } else {
-        prevStopName = "Unknown";
+        prevStopName = 'Unknown';
       }
 
       this.setState({
@@ -56,59 +61,56 @@ class BusInfoModal extends Component {
   };
 
   getMapURL = () => {
-    if (
-      this.props.busInfo.location !== undefined &&
-      this.props.stopInfo.stop_points !== undefined
-    ) {
-      var long = this.props.busInfo.location.lon;
-      var lat = this.props.busInfo.location.lat;
-      var stop_lat = this.props.stopInfo.stop_points[0].stop_lat;
-      var stop_long = this.props.stopInfo.stop_points[0].stop_lon;
-      var mapbox_api = MAPBOX_API_KEY;
-      return `https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-s-bus+3498db(${long},${lat}),pin-s-information+e74c3c(${stop_long},${stop_lat})/auto/400x400@2x?access_token=${mapbox_api}`;
+    const { busInfo, stopInfo } = this.props;
+    if (busInfo.location !== undefined && stopInfo.stop_points !== undefined) {
+      const long = busInfo.location.lon;
+      const { lat } = busInfo.location;
+      const stopLat = stopInfo.stop_points[0].stop_lat;
+      const stopLong = stopInfo.stop_points[0].stop_lon;
+      return `https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-s-bus+3498db(${long},${lat}),pin-s-information+e74c3c(${stopLong},${stopLat})/auto/400x400@2x?access_token=${MAPBOX_API_KEY}`;
     }
+    return null;
   };
 
   render() {
+    const { props, state } = this;
+    // silencing the linter warnings
+    const { isOpen, toggle } = this.props;
     return (
       <div>
-        <Modal
-          isOpen={this.props.isOpen}
-          toggle={e => this.props.toggle()}
-          className="info-modal"
-        >
+        <Modal isOpen={isOpen} toggle={() => toggle()} className="info-modal">
           <ModalHeader toggle={this.toggle}>
-            {this.props.busInfo.headsign}
+            {props.busInfo.headsign}
           </ModalHeader>
           <ModalBody>
             <img
               className="img-fluid map-image"
               alt="bus information"
-              src={this.state.mapURL}
-              style={this.state.imgLoaded ? {} : { visibility: "hidden" }} // Toggle visibility based on the image loaded
+              src={state.mapURL}
+              style={state.imgLoaded ? {} : { visibility: 'hidden' }} // Toggle visibility based on the image loaded
               onLoad={() => this.setState({ imgLoaded: true })}
             />
 
             <p
               className="stop-info"
-              style={this.state.imgLoaded ? {} : { visibility: "hidden" }}
+              style={state.imgLoaded ? {} : { visibility: 'hidden' }}
             >
-              <b>Next Stop:</b> {this.state.nextStop}
+              <b>Next Stop:</b>
+              {state.nextStop}
               <br />
-              <b>Previous Stop:</b> {this.state.previousStop}
+              <b>Previous Stop:</b>
+              {state.previousStop}
               <br />
               <br />
               <i>
-                Position last updated{" "}
-                {Math.round(
-                  (new Date() - new Date(this.state.last_updated)) / 1000
-                )}{" "}
+                Position last updated{' '}
+                {Math.round((new Date() - new Date(state.last_updated)) / 1000)}{' '}
                 seconds ago
               </i>
             </p>
           </ModalBody>
           <ModalFooter>
-            <Button color="secondary" onClick={e => this.props.toggle()}>
+            <Button color="secondary" onClick={() => props.toggle()}>
               Exit
             </Button>
           </ModalFooter>
