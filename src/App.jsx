f6c30d000fe91acc3e1abfb14ce8d5e56026c906
Josh Byster <joshb1050@gmail.com>
2019-05-15 22:58:27 -0500

    Added sentry

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Sentry from '@sentry/browser';
 import './App.css';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import ReactGA from 'react-ga';
@@ -8,6 +9,10 @@ import BusNavbar from './components/BusNavbar';
 
 ReactGA.initialize('UA-109186351-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
+
+Sentry.init({
+  dsn: 'https://a3cd776e555b4f62b9215dee5e11a886@sentry.io/306037'
+});
 
 function App() {
   return (
