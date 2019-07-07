33f50fcd87698399cdd53805f183396335b8ad3b
Josh Byster <joshb1050@gmail.com>
2019-07-07 02:05:49 -0500

    Fixed relative path issue with deployment script

--- backend/deploy_heroku.sh
+++ backend/deploy_heroku.sh
@@ -2,6 +2,7 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
+cd $(dirname "$0")
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
 ## Credit to https://gerardnico.com/vm/docker/ci for the instructions
