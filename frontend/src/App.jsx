fc62329a74f8733fde6e0c89eb84b9ec70bdef79
Josh Byster <joshb1050@gmail.com>
2019-09-06 03:21:18 -0500

    Added logrocket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,19 @@ import ReactGA from 'react-ga';
 import HomePage from './pages/HomePage';
 import TrackingPage from './pages/TrackingPage';
 import BusNavbar from './components/BusNavbar';
+import LogRocket from 'logrocket';
 
 ReactGA.initialize('UA-109186351-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 Sentry.init({
   dsn: 'https://a3cd776e555b4f62b9215dee5e11a886@sentry.io/306037'
+});
+
+LogRocket.init('oh67gs/uiucbus');
+
+Sentry.configureScope(scope => {
+  scope.setExtra('sessionURL', LogRocket.sessionURL);
 });
 
 function App() {
