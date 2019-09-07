d37852b836fd77e521c565e7c2a6790f5e2df415
Josh Byster <joshb1050@gmail.com>
2019-09-07 03:01:50 -0500

    Added conditional include based on environment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,22 +7,29 @@ import HomePage from './pages/HomePage';
 import TrackingPage from './pages/TrackingPage';
 import BusNavbar from './components/BusNavbar';
 import LogRocket from 'logrocket';
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-109186351-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactGA.initialize('UA-109186351-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
-
-Sentry.init({
-  dsn: 'https://a3cd776e555b4f62b9215dee5e11a886@sentry.io/306037'
-});
-
-LogRocket.init('oh67gs/uiucbus');
-
-LogRocket.getSessionURL(sessionURL => {
-  Sentry.configureScope(scope => {
-    scope.setExtra('sessionURL', sessionURL);
+  Sentry.init({
+    dsn: 'https://a3cd776e555b4f62b9215dee5e11a886@sentry.io/306037'
   });
-});
 
+  LogRocket.init('oh67gs/uiucbus');
+
+  LogRocket.getSessionURL(sessionURL => {
+    Sentry.configureScope(scope => {
+      scope.setExtra('sessionURL', sessionURL);
+    });
+  });
+  window.addEventListener('offline', function(e) {
+    console.log('Not connected to LAN');
+  });
+
+  window.addEventListener('online', function(e) {
+    console.log('Connected back on LAN');
+  });
+}
 function App() {
   return (
     <Router>
