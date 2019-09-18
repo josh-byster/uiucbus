113afe6864a016aadbe5613565ffc0282b4a661c
Josh Byster <8664074+josh-byster@users.noreply.github.com>
2019-09-18 15:40:43 -0500

    Add retry logic (#96)
    * Added retry logic
    
    * Cleanup

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,34 +1,43 @@
 import axios from 'axios';
+import promiseRetry from 'promise-retry';
+
 const CUMTD_API_URI =
   process.env.NODE_ENV === 'production'
     ? 'https://bustracker-api.herokuapp.com/api'
     : `http://localhost:${process.env.REACT_APP_API_PORT}/api`;
+
 const MAPBOX_API_KEY =
   'pk.eyJ1Ijoiam9zaC1ieXN0ZXIiLCJhIjoiY2psN2xyZGFoMDY5ZjNxbWtpeDE0dDlwNSJ9.AAJipEPA6e-kLi1Jv3Wpyg';
+
 function getBuses(stopId) {
-  return axios
-    .get(`${CUMTD_API_URI}/getdeparturesbystop?stop_id=${stopId}`)
-    .then(res => res.data);
+  return retryWrapper(`${CUMTD_API_URI}/getdeparturesbystop?stop_id=${stopId}`);
 }
 
 // Get the stop name and additional info
 function getStop(stopId) {
-  return axios
-    .get(`${CUMTD_API_URI}/getstop?stop_id=${stopId}`)
-    .then(res => res.data);
+  return retryWrapper(`${CUMTD_API_URI}/getstop?stop_id=${stopId}`);
 }
 
 function getVehicleInfo(vehicleId) {
-  return axios
-    .get(`${CUMTD_API_URI}/getvehicle?vehicle_id=${vehicleId}`)
-    .then(res => res.data);
+  return retryWrapper(`${CUMTD_API_URI}/getvehicle?vehicle_id=${vehicleId}`);
 }
 
 function getNearestStops(latitude, longitude) {
-  return axios
-    .get(`${CUMTD_API_URI}/getstopsbylatlon?lat=${latitude}&lon=${longitude}`)
-    .then(res => res.data);
+  return retryWrapper(
+    `${CUMTD_API_URI}/getstopsbylatlon?lat=${latitude}&lon=${longitude}`
+  );
 }
+
+function retryWrapper(url) {
+  return promiseRetry(function(retry, number) {
+    if (number > 1) console.error('Retrying XHR with attempt number', number);
+    return axios
+      .get(url)
+      .then(res => res.data)
+      .catch(retry);
+  });
+}
+
 export {
   getBuses,
   getStop,
