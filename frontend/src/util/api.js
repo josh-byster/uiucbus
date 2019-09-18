9d18b8769e7c65d68d0a24929963720ff29faa3f
Josh Byster <joshb1050@gmail.com>
2019-09-18 03:38:01 -0500

    Cleanup

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,52 +1,43 @@
 import axios from 'axios';
-const promiseRetry = require('promise-retry');
+import promiseRetry from 'promise-retry';
 
 const CUMTD_API_URI =
   process.env.NODE_ENV === 'production'
     ? 'https://bustracker-api.herokuapp.com/api'
     : `http://localhost:${process.env.REACT_APP_API_PORT}/api`;
+
 const MAPBOX_API_KEY =
   'pk.eyJ1Ijoiam9zaC1ieXN0ZXIiLCJhIjoiY2psN2xyZGFoMDY5ZjNxbWtpeDE0dDlwNSJ9.AAJipEPA6e-kLi1Jv3Wpyg';
+
 function getBuses(stopId) {
-  return promiseRetry(function(retry, number) {
-    if (number > 1) console.error('Retrying XHR with attempt number', number);
-    return axios
-      .get(`${CUMTD_API_URI}/getdeparturesbystop?stop_id=${stopId}`)
-      .then(res => res.data)
-      .catch(retry);
-  });
+  return retryWrapper(`${CUMTD_API_URI}/getdeparturesbystop?stop_id=${stopId}`);
 }
 
 // Get the stop name and additional info
 function getStop(stopId) {
-  return promiseRetry(function(retry, number) {
-    if (number > 1) console.error('Retrying XHR with attempt number', number);
-    return axios
-      .get(`${CUMTD_API_URI}/getstop?stop_id=${stopId}`)
-      .then(res => res.data)
-      .catch(retry);
-  });
+  return retryWrapper(`${CUMTD_API_URI}/getstop?stop_id=${stopId}`);
 }
 
 function getVehicleInfo(vehicleId) {
+  return retryWrapper(`${CUMTD_API_URI}/getvehicle?vehicle_id=${vehicleId}`);
+}
+
+function getNearestStops(latitude, longitude) {
+  return retryWrapper(
+    `${CUMTD_API_URI}/getstopsbylatlon?lat=${latitude}&lon=${longitude}`
+  );
+}
+
+function retryWrapper(url) {
   return promiseRetry(function(retry, number) {
     if (number > 1) console.error('Retrying XHR with attempt number', number);
     return axios
-      .get(`${CUMTD_API_URI}/getvehicle?vehicle_id=${vehicleId}`)
+      .get(url)
       .then(res => res.data)
       .catch(retry);
   });
 }
 
-function getNearestStops(latitude, longitude) {
-  return promiseRetry(function(retry, number) {
-    if (number > 1) console.error('Retrying XHR with attempt number', number);
-    return axios
-      .get(`${CUMTD_API_URI}/getstopsbylatlon?lat=${latitude}&lon=${longitude}`)
-      .then(res => res.data)
-      .catch(retry);
-  });
-}
 export {
   getBuses,
   getStop,
