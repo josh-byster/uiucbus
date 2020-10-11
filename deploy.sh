840e2af1bc65c93049d38ce1783cc6d7196a0d66
Josh Byster <joshb1050@gmail.com>
2020-10-11 01:57:58 -0500

    Fix strict host checking

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 openssl aes-256-cbc -K $encrypted_dfdcfd5172af_key -iv $encrypted_dfdcfd5172af_iv -in deploy_key.enc -out ./deploy_key -d
 eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
-echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "Host $DO_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
-ssh -o StrictHostKeyChecking=accept-new -i ./deploy_key root@$DO_IP pwd
-ls
+ssh -i ./deploy_key root@$DO_IP pwd
